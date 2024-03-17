04384f0903295027dcbd150428776ca531cc5ef9
Vince489 <vincentcornelius489@gmail.com>
2024-03-17 17:40:59 -0400

    removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,12 +18,7 @@
         <NuxtLink to="#" class="text-slate-200">Art Board</NuxtLink>
         <NuxtLink to="/dashboard" class="text-slate-200">Dashboard</NuxtLink>
       </div>
-      <div v-if="isAuthenticated" class="hidden md:flex items-center space-x-4">
-        <!-- Show Logout link if authenticated -->
-        <NuxtLink to="#" class="text-slate-200" @click="logout">Logout</NuxtLink>
-      </div>
-      <div v-else class="hidden md:flex items-center space-x-4">
-        <!-- Show Login and Sign Up links if not authenticated -->
+      <div class="hidden md:flex items-center space-x-4">
         <NuxtLink to="/login" class="text-slate-200">Login</NuxtLink>
         <NuxtLink to="/signup" class="text-slate-200">Sign Up</NuxtLink>
       </div>
@@ -33,15 +28,8 @@
       <NuxtLink to="#" class="text-slate-200 block py-2">Generate</NuxtLink>
       <NuxtLink to="#" class="text-slate-200 block py-2">About</NuxtLink>
       <NuxtLink to="#" class="text-slate-200 block py-2">Contact</NuxtLink>
-      <template v-if="isAuthenticated">
-        <!-- Show Logout link if authenticated -->
-        <NuxtLink to="#" class="text-slate-200 block py-2" @click="logout">Logout</NuxtLink>
-      </template>
-      <template v-else>
-        <!-- Show Login and Sign Up links if not authenticated -->
-        <NuxtLink to="/login" class="text-slate-200 block py-2">Login</NuxtLink>
-        <NuxtLink to="/signup" class="text-slate-200 block py-2">Sign Up</NuxtLink>
-      </template>
+      <NuxtLink to="/login" class="text-slate-200 block py-2">Login</NuxtLink>
+      <NuxtLink to="/signup" class="text-slate-200 block py-2">Sign Up</NuxtLink>
     </div>
   </nav>
 
@@ -54,12 +42,9 @@
 import { useUserStore } from '~/stores/userStore';
 import { ref, onMounted } from 'vue';
 
-const isAuthenticated = ref('');
-
 const userStore = useUserStore();
-userStore.getUserData().then(() => {
-  isAuthenticated.value = userStore.userData.isAuthenticated;
-});
+
+const isAuthenticated = userStore.isAuthenticated;
 
 let toggle_menu = ref(false);
 
@@ -77,23 +62,7 @@ onMounted(() => {
 function toggleMenu() {
   toggle_menu.value = !toggle_menu.value;
 }
-
-// Define logout function
-async function logout() {
-  try {
-    // Call the logout function from the user store
-    await userStore.logout();
-    // After logout, set isAuthenticated to false
-    isAuthenticated.value = false;
-  } catch (error) {
-    console.error('Error logging out:', error);
-  }
-}
 </script>
-
-<style scoped>
-/* Your styles here */
-</style>
 
 <style scoped>
 /* Additional styling specific to this component */
